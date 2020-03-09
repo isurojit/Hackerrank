cb43c34b1d18f43946a6cb0c5cf314144a82a7c5
isurojit <34436780+isurojit@users.noreply.github.com>
2020-03-09 17:47:40 +0530

    Create loop.py

---
+++ loop.py
@@ -0,0 +1,35 @@
+'''
+Problem:-
+Task
+Read an integer N. For all non-negative integers i< N , print i^2. See the sample for details.
+
+Input Format
+The first and only line contains the integer,N .
+
+Constraints
+1<=N<=20
+
+Output Format
+Print N lines, one corresponding to each .
+
+Sample Input 0
+5
+
+Sample Output 0
+0
+1
+4
+9
+16
+'''
+n = input()
+num = int(n)
+i=0
+while i < num:
+    try:
+        sqr = i * i
+        print(sqr)
+        i = i +1
+    except:
+        print('invalid input')
+        continue
